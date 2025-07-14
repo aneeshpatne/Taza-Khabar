268ee2590ee5d62552fb2058b25302ff0243a39f
aneeshpatne <aneeshpatne@gmail.com>
2025-07-14 18:26:52 +0530

    Implement database connection setup with dotenv configuration and error handling

--- ai/lib/db.js
+++ ai/lib/db.js
@@ -1,2 +1,20 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { Pool } from "pg";
+import * as schema from "./schema/news.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const DATABASE_URL = process.env.DATABASE_URL;
+if (!DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL is not defined. Please check your environment variables."
+  );
+}
+
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+export const db = drizzle(pool, { schema });
